62db1112ce6bff9ee1c80c937a10564602274628
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-05-17 11:26:19 -0400

    submissions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Jessica", "Nora", "Marissa"]
+function writeCards(names){
+    let thankYouCards = []
+    for (let i =0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+    return thankYouCards;
+}
+    writeCards(names);
+    function countDown(number){
+        let countup = (number)
+        while (countup > -1) {
+            console.log(countup--);
+          }
+    }
